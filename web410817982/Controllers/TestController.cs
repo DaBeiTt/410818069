7b835f83f69a08fc576fc7e056cb7d77ed8af06e
蕭念敔 <aa06220912@gmail.com>
2022-04-28 20:42:11 +0800

    修改檔案

--- web410817982/Controllers/TestController.cs
+++ web410817982/Controllers/TestController.cs
@@ -18,5 +18,16 @@ namespace web410817982.Controllers
         {
             return View();
         }
+
+        public ActionResult HW2()
+        {
+            return View();
+        }
+
+        public ActionResult HW3()
+        {
+            return View();
+        }
+
     }
 }
